86c44a93b13f381e09692d160ef3c13dc7e9e8a7
oerdbej <oerdbej@gmail.com>
2023-01-01 14:30:16 +0100

    updating the UI with javascript elements, fetching API works

--- script.js
+++ script.js
@@ -8,5 +8,27 @@ btn.addEventListener("click", () => {
   let inpWord = document.getElementById("inp-word").value;
   fetch(`${url}${inpWord}`)
     .then((response) => response.json())
-    .then((data) => console.log(data));
+    .then((data) => {
+      console.log(data);
+      result.innerHTML = `<div class="word">
+                <h3>${inpWord}</h3>
+                <button>
+                    <i class="fa-solid fa-volume-high"></i>
+                </button>
+            </div>
+            <div class="details">
+                <p>${data[0].meanings[0].partOfSpeech}</p>
+                <p>/${data[0].phonetic}/</p> 
+            </div>
+            <p class="word-meaning">
+        ${data[0].meanings[0].definitions[0].definition}
+            </p>
+            
+            <div class="word-example">
+            <p>${data[0].meanings[0].definitions[0].example || ""}
+</p>
+<br/>
+<p>${data[0].meanings[0].antonyms[0] || ""}</p>  
+</div>`;
+    });
 });
