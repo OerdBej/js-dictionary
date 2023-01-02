2d8fa1df0cadcc439cf36471c6297270d2534a43
oerdbej <oerdbej@gmail.com>
2023-01-02 15:58:34 +0100

    onClick function for the playSound dictionary

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 const url = `https://api.dictionaryapi.dev/api/v2/entries/en/`;
-
 const result = document.getElementById("result");
 const sound = document.getElementById("sound");
 const btn = document.getElementById("search-btn");
@@ -10,12 +9,16 @@ btn.addEventListener("click", () => {
     .then((response) => response.json())
     .then((data) => {
       console.log(data);
-      result.innerHTML = `<div class="word">
+      result.innerHTML = `
+      <div class="word">
                 <h3>${inpWord}</h3>
-                <button>
+
+                <button onclick="playSound()">
                     <i class="fa-solid fa-volume-high"></i>
                 </button>
+
             </div>
+
             <div class="details">
                 <p>${data[0].meanings[0].partOfSpeech}</p>
                 <p>/${data[0].phonetic}/</p> 
@@ -25,10 +28,19 @@ btn.addEventListener("click", () => {
             </p>
             
             <div class="word-example">
-            <p>${data[0].meanings[0].definitions[0].example || ""}
-</p>
+            <p>${data[0].meanings[0].definitions[0].example || ""}</p>
 <br/>
 <p>${data[0].meanings[0].antonyms[0] || ""}</p>  
 </div>`;
+
+      // set song attribute
+
+      sound.setAttribute("src", `${data[0].phonetics[1].audio}`);
+      console.log(sound);
     });
 });
+
+function playSound() {
+  console.log("Click here");
+  sound.play();
+}
